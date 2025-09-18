9d9378c97f704a752bf53681c4c39c6591116eb5
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2025-09-18 14:59:32 -0300

    var x let x const

---
+++ var-let.js
@@ -0,0 +1,12 @@
+// var x let x const 
+
+
+if(true){
+    const myConst = 'Hei'
+    var myVar = 'Hello'
+    let myLet = 'Olá'
+    console.log(myLet)
+    console.log(myConst)
+    console.log(myVar)
+}
+
